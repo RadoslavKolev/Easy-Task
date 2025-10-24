a083b4ec62a0c853e667d6f18ccfa862d2708a08
Radoslav Kolev <radoslav.kolev@abv.bg>
2025-10-24 18:38:41 +0300

    Added Signals for improved rendering

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+const getRandomUser = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,14 +11,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[getRandomUser()]);
+  imagePath = computed(() => `../../assets/users/${this.selectedUser().avatar}`);
 
-  get imagePath() {
-    return `../../assets/users/${this.selectedUser.avatar}`;
-  }
+  //* Since we now use "Signals" we don't need this getter anymore
+  // get imagePath() {
+  //   return `../../assets/users/${this.selectedUser.avatar}`;
+  // }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[getRandomUser()]);
   }
 }
