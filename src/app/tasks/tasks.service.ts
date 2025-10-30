6fc43db18c6541d2566417d95384c68de425a50a
Radoslav Kolev <radoslav.kolev@abv.bg>
2025-10-30 19:04:08 +0200

    Preventing data loss when refreshing the app - saving the tasks in localStorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,21 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    // Returned data from localStorage is always a string
+    // We need to parse it back to an array of tasks
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  // Save the tasks array as a string in localStorage
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -41,9 +56,12 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
   }
 }
